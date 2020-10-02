b7ed5af6d2ef52e47cccde37120e15e4c4492adf
ihassanjavaid <great.maliks@gmail.com>
2020-10-02 15:03:50 +0500

    Add project files.

---
+++ UserManager/EntryPoint.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace UserManager
+{
+    class EntryPoint
+    {
+        static void Main(string[] argv)
+        {
+            Console.WriteLine("Hello!");
+
+        }
+    }
+}
