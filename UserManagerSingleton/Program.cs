b5cf8fd22c84ad76ceb416547cdfc251962c44f1
ihassanjavaid <great.maliks@gmail.com>
2020-10-16 15:01:57 +0500

    Lecture Oct 16, 2020

---
+++ UserManagerSingleton/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserManagerSingleton.Security;
+
+namespace UserManagerSingleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*User user = new User();
+            user.userName = "ABC";
+            User user2 = new User();
+            user2.userName = "XYZ";*/
+
+            User user = User.getInstance();
+            user.userName = "ABC";
+            User user2 = User.getInstance();
+            user2.userName = "MNO";
+            User user3 = User.getInstance();
+            user3.userName = "XYZ";
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ UserManagerSingleton/Security/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserManagerSingleton.Security
+{
+    public class User
+    {
+
+        public string userName { get; set; }
+
+        private User()
+        {
+            Console.WriteLine("Instance " +Count+ " is created!");
+        }
+
+        private static User Instance;
+        private static int Count = 0;
+
+        public static User getInstance()
+        {
+            if (Instance != null)
+                return Instance;
+
+            Count++;
+            Instance = new User();
+            return Instance;
+        }
+    }
+}
